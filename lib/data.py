91821a406f0be7e93099f8ec175c55598d6cc07b
Xuan Duy <moiduy04@gmail.com>
2025-12-01 14:47:16 +0700

    Update code to latest transformers version Also remove lib/ from .gitignore

--- lib/data.py
+++ lib/data.py
@@ -1,48 +1,23 @@
-# Code adapted from https://github.com/IST-DASLab/sparsegpt/blob/master/datautils.py
+import random
 
 import numpy as np
-import random
 import torch
 from datasets import load_dataset
+
 
 # Set seed for reproducibility
 def set_seed(seed):
     np.random.seed(seed)
     torch.random.manual_seed(seed)
 
+
 # Wrapper for tokenized input IDs
 class TokenizerWrapper:
     def __init__(self, input_ids):
         self.input_ids = input_ids
 
-# Load and process wikitext2 dataset
-def get_wikitext2(nsamples, seed, seqlen, tokenizer):
-    # Load train and test datasets
-    traindata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='train')
-    testdata = load_dataset('wikitext', 'wikitext-2-raw-v1', split='test')
 
-    # Encode datasets
-    trainenc = tokenizer(" ".join(traindata['text']), return_tensors='pt')
-    testenc = tokenizer("\n\n".join(testdata['text']), return_tensors='pt')
-
-    # Generate samples from training set
-    random.seed(seed)
-    trainloader = []
-    for _ in range(nsamples):
-        i = random.randint(0, trainenc.input_ids.shape[1] - seqlen - 1)
-        j = i + seqlen
-        inp = trainenc.input_ids[:, i:j]
-        tar = inp.clone()
-        tar[:, :-1] = -100
-        trainloader.append((inp, tar))
-    return trainloader, testenc
-
-# Load and process c4 dataset
-def get_c4(nsamples, seed, seqlen, tokenizer):
-    # Load train and validation datasets
-    traindata = load_dataset('allenai/c4', 'allenai--c4', data_files={'train': 'en/c4-train.00000-of-01024.json.gz'}, split='train')
-    valdata = load_dataset('allenai/c4', 'allenai--c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'}, split='validation')
-
+def prepare_trainloader_valenc(traindata, valdata, seed, nsamples, seqlen, tokenizer):
     # Generate samples from training set
     random.seed(seed)
     trainloader = []
@@ -59,15 +34,85 @@ def get_c4(nsamples, seed, seqlen, tokenizer):
         tar[:, :-1] = -100
         trainloader.append((inp, tar))
 
+    print("generating samples done")
+
     # Prepare validation dataset
     valenc = tokenizer(' '.join(valdata[:1100]['text']), return_tensors='pt')
     valenc = valenc.input_ids[:, :(256 * seqlen)]
     valenc = TokenizerWrapper(valenc)
     return trainloader, valenc
 
+
+# Load and process c4 dataset
+def get_c4(nsamples, seed, seqlen, tokenizer):
+    # Load train and validation datasets
+    traindata = load_dataset(
+        'allenai/c4',
+        data_files={
+            'train': [
+                "en/c4-train.00000-of-01024.json.gz",
+            ]
+        },
+        split='train'
+    )
+
+    print("loading trainset done")
+
+    # valdata = load_dataset('allenai/c4', data_files={'train': 'en/c4-train.00001-of-01024.json.gz'}, split='train')
+    valdata = load_dataset('allenai/c4', data_files={'validation': 'en/c4-validation.00000-of-00008.json.gz'},
+                           split='validation')
+
+    print("loading validset done")
+    return prepare_trainloader_valenc(traindata, valdata, seed, nsamples, seqlen, tokenizer)
+
+
+def get_susi_magical_data(split, nsamples, seed, seqlen, tokenizer):
+    SUSI_MAGICAL_DATA_SOURCE = "/llm-data/hub_data/susi_magical_data/mixture"
+    # Load train and validation datasets
+
+    traindata = load_dataset(
+        "parquet",
+        data_files={'train': f"{SUSI_MAGICAL_DATA_SOURCE}/{split}_part_000000.parquet"},
+        split='train'
+    )
+    print("loading trainset done")
+
+    valdata = load_dataset(
+        "parquet",
+        data_files={'validation': f"{SUSI_MAGICAL_DATA_SOURCE}/{split}_part_000001.parquet"},
+        split='validation'
+    )
+    print("loading validset done")
+
+    return prepare_trainloader_valenc(traindata, valdata, seed, nsamples, seqlen, tokenizer)
+
+
 # Function to select the appropriate loader based on dataset name
-def get_loaders(name, nsamples=128, seed=0, seqlen=2048, tokenizer=None):
-    if 'wikitext2' in name:
-        return get_wikitext2(nsamples, seed, seqlen, tokenizer)
-    if "c4" in name:
+def get_loaders(name, nsamples=128, seed=0, seqlen=None, tokenizer=None):
+    """
+    Returns data loaders based on the dataset name.
+
+    Parameters:
+        name (str): Dataset identifier.
+        nsamples (int): Number of samples to load.
+        seed (int): Random seed for reproducibility.
+        seqlen (int, optional): Sequence length for tokenization.
+        tokenizer (callable, optional): Tokenizer function.
+
+    Returns:
+        DataLoader or similar dataset object.
+    """
+    if "c4" in name.lower():
         return get_c4(nsamples, seed, seqlen, tokenizer)
+
+    if "magic" in name.lower():
+        if "hq" in name.lower():
+            split = "hq"
+        elif "qa" in name.lower():
+            split = "qa"
+        else:
+            raise ValueError(
+                f"Dataset with name '{name}' is a type of 'magic' but no valid split found ('hq' or 'qa').")
+        return get_susi_magical_data(split, nsamples, seed, seqlen, tokenizer)
+
+    raise ValueError(f"Dataset with name '{name}' not found.")
--- main_opt.py
+++ main_opt.py
@@ -8,6 +8,7 @@ from importlib.metadata import version
 from lib.prune_opt import prune_wanda, prune_magnitude, prune_sparsegpt, prune_ablate, check_sparsity, find_layers
 from lib.eval import eval_ppl, eval_zero_shot
 
+
 print('torch', version('torch'))
 print('transformers', version('transformers'))
 print('accelerate', version('accelerate'))
@@ -31,7 +32,7 @@ def main():
     parser.add_argument('--seed', type=int, default=0, help='Seed for sampling the calibration data.')
     parser.add_argument('--nsamples', type=int, default=128, help='Number of calibration samples.')
     parser.add_argument('--sparsity_ratio', type=float, default=0, help='Sparsity level')
-    parser.add_argument("--sparsity_type", type=str, choices=["unstructured", "4:8", "2:4"])
+    parser.add_argument("--sparsity_type", type=str)
     parser.add_argument("--prune_method", type=str, choices=["magnitude", "wanda", "sparsegpt", 
                         "ablate_mag_seq", "ablate_wanda_seq", "ablate_mag_iter", "ablate_wanda_iter", "search"])
     parser.add_argument("--cache_dir", default="llm_weights", type=str )
@@ -49,7 +50,7 @@ def main():
     # Handling n:m sparsity
     prune_n, prune_m = 0, 0
     if args.sparsity_type != "unstructured":
-        assert args.sparsity_ratio == 0.5, "sparsity ratio must be 0.5 for structured N:M sparsity"
+        # assert args.sparsity_ratio == 0.5, "sparsity ratio must be 0.5 for structured N:M sparsity"
         prune_n, prune_m = map(int, args.sparsity_type.split(":"))
 
     model_name = args.model.split("/")[-1]
@@ -80,27 +81,6 @@ def main():
     print(f"sparsity sanity check {sparsity_ratio:.4f}")
     print("*"*30)
     ################################################################
-    ppl_test = eval_ppl(args, model, tokenizer, device)
-    print(f"wikitext perplexity {ppl_test}")
-
-    if not os.path.exists(args.save):
-        os.makedirs(args.save)
-    save_filepath = os.path.join(args.save, f"log_{args.prune_method}.txt")
-    with open(save_filepath, "w") as f:
-        print("method\tactual_sparsity\tppl_test", file=f, flush=True)
-        print(f"{args.prune_method}\t{sparsity_ratio:.4f}\t{ppl_test:.4f}", file=f, flush=True)
-
-    if args.eval_zero_shot:
-        accelerate=False
-        if "30b" in args.model or "66b" in args.model:
-            accelerate=True
-
-        task_list = ["boolq", "rte","hellaswag","winogrande", "arc_easy","arc_challenge", "openbookqa"]
-        num_shot = 0
-        results = eval_zero_shot(args.model, model, tokenizer, task_list, num_shot, accelerate)
-        print("********************************")
-        print("zero_shot evaluation results")
-        print(results)
 
     if args.save_model:
         model.save_pretrained(args.save_model)
